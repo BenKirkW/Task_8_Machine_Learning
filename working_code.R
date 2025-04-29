7ee9a6211736df50d930ee1bde593fa557b980c6
Ben Wildey <benwildey8@gmail.com>
2025-04-29 10:26:55 +0000

    git hub set up

--- working_code.R
+++ working_code.R
@@ -5,6 +5,25 @@ library(readxl)
 library(tidyverse)
 library(here)
 library(performance)
+library(usethis)
+library(gitcreds)
+library(tidyverse)
+library(emmeans)
+library(janitor)
+library(see)
+
+
+#setting up github on the new work space----
+#use_git_config(user.name  = "Ben Wildey",
+#user.email = "benwildey8@gmail.com")
+
+#usethis::create_github_token()
+#used to make the token 
+
+#gitcreds::gitcreds_set()
+#used to set the token
+#________________----
+
 
 # Read the dataset into R
 ch4 <- read_xlsx(here("data", "DNA methylation data.xlsm"), sheet = 1)
