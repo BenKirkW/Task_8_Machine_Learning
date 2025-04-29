28e0d939c2216bce3f6f408aa1fa4cd09a02ff4c
Ben Wildey <benwildey8@gmail.com>
2025-04-29 16:08:47 +0000

    seperated the year and site into seperate coloumns and checking the missing data

--- working_code.R
+++ working_code.R
@@ -30,3 +30,27 @@ ch4 <- read_xlsx(here("data", "DNA methylation data.xlsm"), sheet = 1)
 
 # Explore the first few rows
 head(ch4)
+
+
+# Create the bar graph to check age distribution of samples
+ggplot(ch4, aes(x = factor(Age), y = frequency(Age))) +
+  geom_bar(stat = "identity") +
+  labs(title = "Number of Samples for Each Age",
+       x = "Age",
+       y = "Number of Samples") +
+  theme_minimal()
+
+#checking methylation values are all between 0-100
+summary(ch4)
+
+
+
+# Separate the column into two new columns
+ch4_clean <- ch4 %>% separate(Sample, into = c("Location", "Year"), sep = "[_-]")
+
+
+#checking for missing data %'s
+
+# Calculate the percentage of missing values for each column
+column_missing_percentage <- colMeans(is.na(ch4)) * 100
+print(column_missing_percentage)
